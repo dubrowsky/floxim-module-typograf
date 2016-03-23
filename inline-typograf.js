89e64e5062d5c40ec8f4d3c06a49e102457b6682
Ilya <dubr.cola@gmail.com>
2016-03-23 19:28:10 +0300

    fixed

--- inline-typograf.js
+++ inline-typograf.js
@@ -33,13 +33,16 @@ $(function() {
     var generate_position = function(node, parent) {
         var res = [];
         while (node !== parent) {
+            if (node.parentNode) {
+                node.parentNode.normalize();
+            }
             var index = -1,
                 sibling = node;
             while ( sibling ) {
                 index++;
                 sibling = sibling.previousSibling;
+                //console.log(index, sibling);
             }
-            
             res.unshift(index);
             node = node.parentNode;
         }
@@ -76,27 +79,48 @@ $(function() {
         return res;
     };
     
-    function enableTypograf($elem) {
-        var elem_is_input = $elem.is('input, textarea');
-        if (elem_is_input) {
-            var getValue = function() {
-                return $elem.val();
-            };
-            var setValue = function(val) {
-                $elem.val(val);
-            };
-        } else {
-            var getValue = function() {
-                return $elem.html();
-            };
-            var setValue = function(val) {
-                $elem.html(val);
-            };
+    function set_cursor_position_in_input(input, pos) {
+        if (input.setSelectionRange) {
+            input.setSelectionRange(pos, pos);
+        } 
+        // IE < 9
+        else if (input.createTextRange) {
+            var range = input.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+        }
+    }
+    
+    function get_cursor_position_in_input(input, pos) {
+        
+        if (typeof input.selectionStart !== 'undefined') {
+            return input.selectionStart;
         }
         
-        var lastVal = getValue(),
+        // should help IE < 9, not really tested
+        if (document.selection) {
+            input.focus();
+            var sel = document.selection.createRange();
+            sel.moveStart('character', input.value.length * -1);
+            return sel.text.length;
+        }
+    }
+    
+    function enable_typograf($elem) {
+        var elem_is_input = $elem.is('input, textarea'),
             elem = $elem[0];
         
+        // don't try to use ternary operator, it breaks hilighting here =)
+        if (elem_is_input) {
+            var get_value = function() {return $elem.val();};
+        } else {
+            var get_value = function() {return $elem.html();};
+        }
+        
+        var last_val = get_value();
+            
         if ($elem.data('has_typograf')) {
             return;
         }
@@ -104,9 +128,9 @@ $(function() {
         $elem.data('has_typograf', true);
         
         $elem
-            .on('input.typograph', function(e) {
-                var val = getValue();
-                if (val === lastVal) {
+            .on('input.typograph, paste.typograph', function(e) {
+                var val = get_value();
+                if (val === last_val) {
                     return;
                 }
                 if (val.length > 10000) {
@@ -114,56 +138,57 @@ $(function() {
                     return;
                 }
                 
-                var selection = window.getSelection(),
-                    range = selection.getRangeAt(0);
+                
+                if (elem_is_input) {
+                    var res = typograf.execute(val);
+                    if (res === val) {
+                        return;
+                    }
+                    var c_pos = get_cursor_position_in_input(elem);
+                    $elem.val(res);
+                    last_val = res;
+                    set_cursor_position_in_input(elem, c_pos);
+                } else {
+                    //debugger;
+                    var selection = window.getSelection(),
+                        range = selection.getRangeAt(0);
+
+                    range.startContainer.parentNode.normalize();
                     
-                range.startContainer.parentNode.normalize();
-                
-                var marker_offset = 0;
-                if (val.charCodeAt(val.length - 1) === 8203) {
-                    marker_offset = 1;
-                    val = val.replace(/.$/, '');
-                }
-                var res = typograf.execute(val);
-                
-                if (res === to_entities(val)) {
-                    lastVal = res;
-                    return;
-                }
-                
-                lastVal = res;
+                    if (!range.collapsed) {
+                        return;
+                    }
 
-                
-                
-                var start = range.startOffset;
-                
-                var inv_start = range.startContainer.length - start - marker_offset;
-                
-                if (!range.collapsed) {
-                    return;
-                }
+                    var marker_offset = 0;
+                    if (val.charCodeAt(val.length - 1) === 8203) {
+                        marker_offset = 1;
+                        val = val.replace(/.$/, '');
+                    }
+                    var res = typograf.execute(val);
 
-                if (!elem_is_input) {
+                    if (res === to_entities(val)) {
+                        last_val = res;
+                        return;
+                    }
+
+                    last_val = res;
+
+                    var start = range.startOffset;
+                    var inv_start = range.startContainer.length - start - marker_offset;
+                    
                     var pos = generate_position(range.startContainer, elem);
-                }
-
-                setValue(res);
-
-                if (!elem_is_input) {
+                    
+                    $elem.html(res);
+                    
                     var new_node = find_by_position(pos, elem);
-
                     if (new_node) {
                         var new_range = document.createRange();
                         new_range.setStart(new_node, new_node.length - inv_start);
                         new_range.collapse(true);
-
                         selection.removeAllRanges();
                         selection.addRange(new_range);
                     }
-                } else {
-                    //console.log(start, range);
                 }
-                    
             });
     }
 
@@ -171,7 +196,7 @@ $(function() {
         var $node = $(e.target),
             meta = $node.data('fx_var');
         if (meta) {  
-            enableTypograf($(e.target));
+            enable_typograf($(e.target));
         }
     });
     
@@ -183,7 +208,7 @@ $(function() {
         }
         var $fields = $('.redactor_fx_wysiwyg, .fx_input_string', $form);
         $fields.each(function() {
-            enableTypograf($(this));
+            enable_typograf($(this));
         });
     });
 });
